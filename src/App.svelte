554a6e605e5c8ff0ae7063f3e6cc49fdbc75eedb
Erlend Johann Skarpnes <erlend.skarpnes@miles.no>
2025-05-22 12:57:04 +0200

    better card and board sizing

--- src/App.svelte
+++ src/App.svelte
@@ -30,49 +30,51 @@
 </script>
 
 <svelte:window bind:innerWidth={screenWidth}/>
-<main>
-    <div class="actions" bind:this={anchorEl}>
-        {#if $gameState.deck.length > 0}
-            <div class="deck">
-                <Cardback onClick={game.draw} />
+    <main>
+        <div class="actions" bind:this={anchorEl}>
+            {#if $gameState.deck.length > 0}
+                <div class="deck">
+                    <Cardback onClick={game.draw} />
+                </div>
+            {/if}
+            <div>
+                <button onclick={game.resetGame} disabled={$gameState.isResetting}>Nytt spill</button>
             </div>
-        {/if}
-        <div>
-            <button onclick={game.resetGame} disabled={$gameState.isResetting}>Nytt spill</button>
         </div>
-    </div>
-    <div class="play-area">
-        {#each $gameState.board as column, columnIndex}
-            <div class="column {($gameState.activeColumn === columnIndex) && !$gameState.isResetting ? 'active' : ''}" id="column-{columnIndex}">
-                {#each column as card, stackIndex (card)}
-                    <Card value={card} stackIndex={stackIndex} picked={$gameState.activeCards.includes(card)}
-                          onClick={() => {
-                          toggleActiveCard(card)
-                          }}
-                          transitionStartElement={anchorEl}
-                          active={$gameState.activeColumn === columnIndex}
-                    />
-                {/each}
-            </div>
-        {/each}
-    </div>
-    {#if $gameState.gameState === "won"}
-        <p>Du vant!</p>
-    {/if}
-    {#if $gameState.gameState === "lost"}
-        <p>Du tapte!</p>
-    {/if}
-</main>
+        <div class="play-area">
+            {#each $gameState.board as column, columnIndex}
+                <div class="column {($gameState.activeColumn === columnIndex) && !$gameState.isResetting ? 'active' : ''}" id="column-{columnIndex}">
+                    {#each column as card, stackIndex (card)}
+                        <Card value={card} stackIndex={stackIndex} picked={$gameState.activeCards.includes(card)}
+                              onClick={() => {
+                              toggleActiveCard(card)
+                              }}
+                              transitionStartElement={anchorEl}
+                              active={$gameState.activeColumn === columnIndex}
+                        />
+                    {/each}
+                </div>
+            {/each}
+        </div>
+        {#if $gameState.gameState === "won"}
+            <p>Du vant!</p>
+        {/if}
+        {#if $gameState.gameState === "lost"}
+            <p>Du tapte!</p>
+        {/if}
+    </main>
 
 <style>
     :global(:root) {
-        --card-width: clamp(140px, 15vw, 200px);
+        --card-width: clamp(120px, 8vw, 180px);
         --card-aspect-ratio: 2.5 / 3.5; /* Standard playing card ratio */
     }
 
     main {
         display: flex;
         flex-direction: row;
+        width: fit-content;
+        max-width: 100%;
     }
 
     .actions {
@@ -88,14 +90,11 @@
         display: grid;
         grid-template-columns: repeat(8, var(--card-width));
         overflow-x: scroll;
+        scrollbar-width: none;
     }
 
     .play-area::-webkit-scrollbar {
         display: none;
-    }
-
-    .play-area {
-        scrollbar-width: none;
     }
 
     .column {
@@ -115,5 +114,4 @@
         width: 100%;
         flex-shrink: 0; /* Prevent shrinking */
     }
-
 </style>
--- src/lib/Cardback.svelte
+++ src/lib/Cardback.svelte
@@ -19,22 +19,33 @@
     const {onClick}: {onClick: () => void} = $props();
 </script>
 
-<button onclick={onClick}>
-    <img src="cardbacks/{randomCardBack}" alt="card back" />
-</button>
+<div class="cardback-container">
+    <button onclick={onClick}>
+        <img src="cardbacks/{randomCardBack}" alt="card back" />
+    </button>
+</div>
 
 <style>
+    .cardback-container {
+        flex-shrink: 0; /* Prevent shrinking */
+        width: 100%;
+    }
+    
     img {
+        box-sizing: border-box;
         width: 100%;
         display: block;
+        aspect-ratio: var(--card-aspect-ratio);
     }
 
     button {
         all: unset;
+        width: 100%;
         padding: 0;
         display: block;
         cursor: pointer;
         transition: box-shadow 0.1s;
+        flex-shrink: 0; /* Prevent shrinking */
     }
 
     button:hover {
